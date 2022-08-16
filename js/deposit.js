50481a9a0e63bfda4b7c2663add9e17db6506e4e
Mezan2002 <mezanurr009@gmail.com>
2022-08-17 02:18:42 +0600

    Deposit is almost done

---
+++ js/deposit.js
@@ -0,0 +1,12 @@
+// step 1 : add event listener on the button
+document.getElementById("deposit_btn").addEventListener("click", function () {
+  const depositInput = document.getElementById("deposit_input");
+  const newDepositValueString = depositInput.value;
+  const newDepositValue = parseFloat(newDepositValueString);
+  depositInput.value = "";
+  const depositInputAmount = document.getElementById("deposit_total");
+  const depositInputAmountString = depositInputAmount.innerText;
+  const depositInputResult = parseFloat(depositInputAmountString);
+  const newDepositTotal = depositInputResult + newDepositValue;
+  depositInputAmount.innerText = newDepositTotal;
+});
